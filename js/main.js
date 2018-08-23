1dd3e77106dcbe18952028a7c40205f479b2aa44
stephenkairos <stephenkairos.reyes@sjsu.edu>
2018-08-22 22:59:24 -0700

    Updated Character Sheet, Logic
    * Added Functional Logic for Calculations
    * GASTON!
    * Fixed Active marker for list element in profile
    * Cleaned up main.js, one Vue for now

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
-var app = new Vue({
-	  el: '#app',
-	  data: {
-	    message: 'Hello Vue!'
-	  }
-	})
-
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-})
-
 var characterApp = new Vue({
   el: '#character-app',
   data: {
     charName: 'Gulliver Coalman',
     levelNumber: 1,
-    notableSkill: 'Smithing'
+    notableSkill: 'Smithing',
+    profileAddress: 'res/img/gaston-profile.jpg',
+    bodyStat: 1,
+    mindStat: 3,
+    soulStat: 1
+  },
+  computed: {
+    agilityStat: function() {
+      agility = (this.bodyStat + this.mindStat) / 2;
+      return agility;
+    },
+    healthStat: function() {
+      var health = (this.bodyStat + this.soulStat) / 2;
+      return health;
+    },
+    willpowerStat: function() {
+      var willpower = (this.soulStat + this.mindStat) / 2;
+      return willpower;
+    },
   }
 })
